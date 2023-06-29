61e377b34d84093a46afeaa44d8410e00c6527fb
Nshoan Abdlwafa <nshoan.abdlwafa@chasacademy.com>
2023-06-29 14:49:19 +0200

    created User class

---
+++ Movie-system/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace Movie_system.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string? UserName { get; set; }
+        public string? UserEmailAdress { get; set; }
+    }
+}
