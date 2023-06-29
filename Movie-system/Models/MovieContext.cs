5db0c0048c1b65d59e393c283f9e9c34db239593
Nshoan Abdlwafa <nshoan.abdlwafa@chasacademy.com>
2023-06-29 11:23:29 +0200

    added MovieContext

---
+++ Movie-system/Models/MovieContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Movie_system.Models
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options)
+            : base(options)
+        {
+        }
+        DbSet<Movie> Movies { get; set; } = null!;
+    }
+}
