8077c6315e44f4193156a78af3c81f7928a2702b
Nshoan Abdlwafa <nshoan.abdlwafa@chasacademy.com>
2023-06-29 16:34:19 +0200

    added Genre model

---
+++ Movie-system/Models/Genre.cs
@@ -0,0 +1,11 @@
+﻿namespace Movie_system.Models
+{
+    public class Genre
+    {
+        public int GenreId { get; set; }
+        public string? GenreTitle { get; set; }
+        public string? GenreDescription { get; set; }
+        public int UserId { get; set; }
+        public string? UserName { get; set; }
+    }
+}
