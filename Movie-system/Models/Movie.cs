40a60aba8afd6dbd7fac3ec222b3bdb8f18abbf7
Nshoan Abdlwafa <nshoan.abdlwafa@chasacademy.com>
2023-06-29 11:18:36 +0200

    created Models folder and added first model

---
+++ Movie-system/Models/Movie.cs
@@ -0,0 +1,10 @@
+﻿namespace Movie_system.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Genre { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
