2ba1e6c69deec67403b94e30261ece7905e65810
Nshoan Abdlwafa <nshoan.abdlwafa@chasacademy.com>
2023-06-29 15:42:18 +0200

    deleted redudant files and made some changes to Context model

--- Movie-system/Models/MovieSystemContext.cs
+++ Movie-system/Models/MovieSystemContext.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using Movie_system.Migrations;
 
 namespace Movie_system.Models
 {
@@ -10,5 +13,13 @@ namespace Movie_system.Models
         }
         public DbSet<Movie> Movies { get; set; } = null!;
         public DbSet<User> Users { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=ASPIRE;Initial Catalog=MovieSystemDB;Integrated Security=True;");
+            }
+        }
     }
 }
