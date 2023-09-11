e5e1dd50eee08e711039335138815f7aa858e3ce
Nshoan Abdlwafa <nshoan.abdlwafa@chasacademy.com>
2023-09-11 18:44:19 +0200

    created TMDBController which contains a method for TMDB recommendations

---
+++ Movie-system/Controllers/TMDBController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Movie_system.Models;
+
+namespace Movie_system.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TMDBController : ControllerBase
+    {
+        private readonly MovieSystemContext _dbContext;
+        public TMDBController(MovieSystemContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet("TMDBDiscover/{TmdbId}")]
+        public async Task<ActionResult> TMDBDiscover(int TmdbId)
+        {
+            var TmdbUrl = $" https://api.themoviedb.org/3/discover/movie?api_key=6515a73f1ebd8a6b92919c33fab29510&with_genres={TmdbId}";
+
+            HttpClient client = new HttpClient(); // Sends a HTTP request to TMDB and awaits an answer
+            HttpResponseMessage response = await client.GetAsync(TmdbUrl);
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            return Ok(responseBody);
+        }
+    }
+}
