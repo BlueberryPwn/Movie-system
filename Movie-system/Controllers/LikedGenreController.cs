137e941f891c2e0fe3159ba7f2b6156478954dc1
Nshoan Abdlwafa <nshoan.abdlwafa@chasacademy.com>
2023-09-11 11:10:34 +0200

    created LikedGenreController

---
+++ Movie-system/Controllers/LikedGenreController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Movie_system.Models;
+
+namespace Movie_system.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LikedGenreController : ControllerBase
+    {
+        private readonly MovieSystemContext _dbContext;
+        public LikedGenreController(MovieSystemContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+    }
+}
